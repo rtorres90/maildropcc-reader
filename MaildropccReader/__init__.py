17ee8e4785103f405dabfbe32821014bf78fa249
Roberto Torres <roberto.torres.dev@gmail.com>
2017-07-15 10:44:18 -0700

    comments and better description on main module

--- MaildropccReader/__init__.py
+++ MaildropccReader/__init__.py
@@ -1,3 +1,4 @@
+"This module contains the MaildropccReader class."
 import json
 
 import requests
@@ -7,36 +8,23 @@ class MaildropccReader(object):
     MaildropccReader is a simple API to extract mail account from www.maildrop.cc
 
     >>> from MaildropccReader import MaildropccReader
-    >>> maildrop_reader = MaildropccReader(email_account="test123")
+    >>> maildrop_reader = MaildropccReader(email_account="chile")
     >>> maildrop_reader.get_emails()
     Output:
-    [{'body': u'\nHi there,\nAs part of ATTN:\'s continuing coverage of America\'s heroin epidemic, we published an interview this week with U.S. Sen. Angus King, an independent from Maine, to discuss solutions to the disturbing number of deaths from opioids in his state. Roughly one person dies from an overdose per day in Maine. \nKing told ATTN: that doctors bear some responsibility for the crisis. In fact, four out of five new heroin users start with prescription drugs, many of which are first obtained legally. "One of the way doctors and hospitals are now measured is: \'Did you leave the office pain free?,\'" King told ATTN:. "The problem is that becomes an incentive to prescribe these very powerful pain medications." King believes that we need to train doctors to better prevent addiction and to specify more clearly what quantity of pills should be prescribed for common injuries. You can watch our entire interview here.\nOther Stories Worth Your Attention This Week: \n+ \nWhat Happens When You Put Disturbing Labels on Cigarette Packs\n+ \nWho Benefits From the Republican Plan to Expand Drug Testing\n+ \n3 Major Takeaways from Congress\' Hearing on Russia and the U.S. Election\n+ \nAl Gore on How Climate Change is Fueling the Refugee Crisis\n+ \nNo One Can Deny: Meals on Wheels Gets Results\nTalk to you soon,\nMatthew\n\nMake sure you never miss out on Sundays - drag this email to your primary box!\nThis email was sent to: test@maildrop.cc\nTo unsubscribe, click Here.\n\n',
-      'email_url': u'https://www.maildrop.cc/inbox/test/PA7Bu4',
-      'raw_row': <tr data-id="PA7Bu4">\n<td class="sender">delivery@mx.sailthru.com</td>\n<td class="subject"><a href="/inbox/test/PA7Bu4">How pills contribute to this epidemic</a></td>\n<td class="date">Mar 26 2017 05:48 PM</td>\n</tr>,
-      'reception_date': u'Mar 26 2017 05:48 PM',
-      'sender': u'delivery@mx.sailthru.com',
-      'subject': u'How pills contribute to this epidemic'},
-     {'body': u"\n\n\n\n\n\n\n\n\n\n\n\n\nPseudo : elyane\nAge : 21 ans\nLocalisation : \xe0 moins de 24 km\nActuellement : En ligne \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPseudo : emilia\nAge : 23 ans\nLocalisation : \xe0 moins de 24 km\nActuellement : En ligne \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPseudo : alessia\nAge : 35 ans\nLocalisation : \xe0 moins de 24 km\nActuellement : En ligne \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPseudo : Ai\nAge : 23 ans\nLocalisation : \xe0 moins de 24 km\nActuellement : En ligne \n\n\n\n\n\n\n\n\n\n\n\n\n\nVous recevez cet email car vous \xeates inscrit sur la mailing liste d'un de nos sites. Pour vous d\xe9sinscrire : http://www.moninfoforte.com/index.php/lists/vm583ee7sf624/unsubscribe/CcQk957VmVx6b/ep8406zg3a11e\n\n",
-      'email_url': u'https://www.maildrop.cc/inbox/test/8shnnk',
-      'raw_row': <tr data-id="8shnnk">\n<td class="sender">webmaster@moninfoforte.com</td>\n<td class="subject"><a href="/inbox/test/8shnnk">Coquines en ligne vite!</a></td>\n<td class="date">Mar 26 2017 12:21 PM</td>\n</tr>,
-      'reception_date': u'Mar 26 2017 12:21 PM',
-      'sender': u'webmaster@moninfoforte.com',
-      'subject': u'Coquines en ligne vite!'},
-     {'body': u"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nVous recevez cet email car vous \xeates inscrit sur la mailing liste d'un de nos sites. Pour vous d\xe9sinscrire : http://www.moninfoforte.com/index.php/lists/vm583ee7sf624/unsubscribe/CcQk957VmVx6b/fz81163e04f35\n\n",
-      'email_url': u'https://www.maildrop.cc/inbox/test/IujDLk',
-      'raw_row': <tr data-id="IujDLk">\n<td class="sender">webmaster@moninfoforte.com</td>\n<td class="subject"><a href="/inbox/test/IujDLk">Vous avez 5 nouvelles demandes d'amies</a></td>\n<td class="date">Mar 26 2017 12:27 PM</td>\n</tr>,
-      'reception_date': u'Mar 26 2017 12:27 PM',
-      'sender': u'webmaster@moninfoforte.com',
-      'subject': u"Vous avez 5 nouvelles demandes d'amies"}
-      ]
+    [{
+        'id':'aTC7Qx',
+        'sender': u'test@ismyemailworking.com',
+        'subject':'IsMyEmailWorking.com - Test ID [c8a64c21-2c1b-463d-8bf0-1283f8e16f92]',
+        'date': u'Jul 15 2017 10:30 AM',
+        'body':'Received: from IsMyEmailWorking.com ([108.60.212.145])\r\n        by localhost\r\n        with SMTP (MailDrop) id J55KIM4A\r\n        for chile@maildrop.cc;\r\n        Sat, 15 Jul 2017 10:30:25 -0700 (PDT)\r\nMIME-Version: 1.0\r\nMessage-ID: <afe6e0fd7cfe899d@b0467bdd42a8d410>\r\nDate: Sat, 15 Jul 2017 10:30:58 -0700\r\nFrom: "Email Test" <test@ismyemailworking.com>\r\nTo: chile@maildrop.cc\r\nSubject: IsMyEmailWorking.com - Test ID [c8a64c21-2c1b-463d-8bf0-1283f8e16f92]\r\nContent-Type: text/plain;\r\n\tcharset="utf-8"\r\nContent-Transfer-Encoding: quoted-printable\r\n\r\nCongratulations=20you=20have=20now=20proven=20that=20you=20at=20least=20rec=\r\neive=20email!=20Now=20to=20prove=20that=20you=20can=20also=20send=20email=\r\n=20simply=20reply=20to=20this=20message=20and=20we=20will=20let=20you=20kno=\r\nw=20when=20we=20receive=20your=20reply.=20Be=20sure=20to=20check=20your=20j=\r\nunk/bulk/spam=20(whatever=20it=20is=20for=20you)=20box=20again=20for=20our=\r\n=20next=20reply.=0d=0a=0d=0a=0d=0aPlease=20note,=20we=20do=20not=20spam=20o=\r\nr=20solicit=20anyone.=20You=20received=20this=20email=20because=20you=20(or=\r\n=20someone=20posing=20to=20be=20you)=20requested=20it.=20We=20implement=20a=\r\nnd=20work=20to=20constantly=20improve=20our=20security=20technologies=20in=\r\n=20order=20to=20prevent=20non-humans=20and=20others=20from=20using=20your=\r\n=20email=20address=20in=20our=20service=20while=20at=20the=20same=20time=20=\r\nothers=20may=20work=20constantly=20to=20defeat=20our=20security.=20If=20you=\r\n=20have=20received=20this=20email=20and=20did=20not=20request=20us=20please=\r\n=20contact=20us=20via=20the=20contact=20link=20on=20our=20website=20at=20ht=\r\ntp://IsMyEmailWorking.com/Contact.aspx.\r\n'
+    }]
     Output structure.
 
     The output is a dictionary with the followind data:
 
+    - id => email id on maildrop.cc platform.
     - body => email content.
-    - email_url => url to access the email data.
-    - raw_row => raw html row of the email.
-    - reception_date => reception date of the email.
+    - date => reception date of the email.
     - sender => sender email.
     - subject => Subject of the email.
     """
@@ -49,11 +37,12 @@ class MaildropccReader(object):
         return "<Maildrop[%s]>" % self.email_account
 
     def get_raw_emails_from_api(self):
+        "gets raw email data from maildrop api."
         request = requests.get(self._maildrop_domain + self.email_account)
         return json.loads(request.text)
 
     def get_raw_email_body(self, email_id):
-        """Extracts the email content of the provided email url"""
+        """Extracts the email content of the provided email url."""
         request = requests.get(self._maildrop_domain + self.email_account + "/" + email_id)
         return json.loads(request.text)
 
@@ -63,14 +52,17 @@ class MaildropccReader(object):
 
         for raw_email in raw_emails:
             raw_email_body = self.get_raw_email_body(raw_email.get('id'))
-            raw_email.update({'body': raw_email_body})
+            raw_email.update({'body': raw_email_body.get('body')})
         return raw_emails
 
     def get_emails_by_sender(self, sender):
+        "filters emails by sender"
         return [mail for mail in self.get_emails() if sender in mail.get('sender')]
 
     def get_emails_by_subject(self, subject):
+        "filters emails by subject"
         return [mail for mail in self.get_emails() if subject in mail.get('subject')]
 
     def get_emails_by_content(self, content):
+        "filters emails by content(body)"
         return [mail for mail in self.get_emails() if content in mail.get('body')]
