1410530dfaf612074a16283b0f931a1f44a909a1
Roberto Torres <Roberto.Torres@onbase.com>
2017-08-07 12:22:55 -0700

    preparing everything for new rev

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name='MaildropccReader',
   packages=['MaildropccReader'],
-  version='1.0.1',
-  description='Simple API to read maildrop.cc emails',
+  version='1.1.0',
+  description='Simple API to extract emails of accounts from www.maildrop.cc',
   author='Roberto Torres',
   author_email='roberto.torres.dev@gmail.com',
   url='https://github.com/rtorres90/maildropcc-reader',
-  download_url='https://github.com/rtorres90/maildropcc-reader/archive/1.0.1.tar.gz',
+  download_url='https://github.com/rtorres90/maildropcc-reader/archive/1.1.0.tar.gz',
   keywords=['api', 'maildropcc', 'testing'],
   install_requires=[
     'requests'
